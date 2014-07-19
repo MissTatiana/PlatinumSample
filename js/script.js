64d80f9db1e346ddc27be34d2282820ac13cb98c
Tatiana Kerick <KerickTatiana@gmail.com>
2014-07-19 02:52:15 -0400

    styling finished, slider functioning, working on responsive

--- js/script.js
+++ js/script.js
@@ -11,21 +11,45 @@ jQuery(document).ready( function ($) {
 
 
 //Dropdown
-$(".dropdown dt a").click(function() {
-    $(".dropdown dd ul").toggle();
+$('.dropdown dt a').click(function() {
+    $('.dropdown dd ul').toggle();
 });
 
-$(".dropdown dd ul li a").click(function() {
+$('.dropdown dd ul li a').click(function() {
     var text = $(this).html();
-    $(".dropdown dt a span").html(text);
-    $(".dropdown dd ul").hide();
+    $('.dropdown dt a span').html(text);
+    $('.dropdown dd ul').hide();
 }); 
 
 $(document).bind('click', function(e) {
     var $clicked = $(e.target);
-    if (! $clicked.parents().hasClass("dropdown"))
-        $(".dropdown dd ul").hide();
+    if (! $clicked.parents().hasClass('dropdown'))
+        $('.dropdown dd ul').hide();
 });
+
+
+//Carousel
+$('.banner').unslider({
+	speed: 500
+});
+
+var unslider = $('.banner').unslider();
+
+$('#prev').click(function(e) {
+	e.preventDefault();
+
+	unslider.data('unslider').prev()
+});
+
+$('#next').click(function(e) {
+	e.preventDefault();
+
+	unslider.data('unslider').next()
+})
+
+
+
+
 
 
 });//end document
